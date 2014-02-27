772a98dd7c6d4282f9248381d191d9a4c79a21a2
ansds <ans8d.s@gmail.com>
2014-02-27 18:58:44 +0200

    implemented quartz scheduler with jms

--- src/main/java/org/schedulerjms/infrastructure/quartzhandler/EventJob.java
+++ src/main/java/org/schedulerjms/infrastructure/quartzhandler/EventJob.java
@@ -1,27 +1,14 @@
 package org.schedulerjms.infrastructure.quartzhandler;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
 import org.json.JSONObject;
 import org.quartz.*;
 import org.schedulerjms.infrastructure.jms.sender.MessageSender;
-import org.schedulerjms.infrastructure.jms.sender.MessageSenderImpl;
-import org.schedulerjms.springfactory.SpringFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jms.core.JmsTemplate;
+import org.schedulerjms.springfactory.*;
 
 /**
  * Created by dSklyarenko on 25.02.14.
  */
 public class EventJob implements Job {
-
-//    @Autowired
-//    @Qualifier("messageSender")
-    //   private ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
-    // private JmsTemplate jmsTemplate = new JmsTemplate(activeMQConnectionFactory);
 
     private MessageSender messageSender;// = new MessageSenderImpl();
 
